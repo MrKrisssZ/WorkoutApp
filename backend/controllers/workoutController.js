1cd7c8fc0f5414b48e82fdbcda2894a08cf5bed3
Yesheng Zhong <yesheng.zhong@student.adelaide.edu.au>
2024-05-10 21:20:49 +0930

    Finish controller setup and solve some error
    Error occur when a asynchronous function created without await method, which means javascript does not wait for update to complete and continue executing.

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,103 @@
+const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
+
+
+// get all workouts
+const getWorkouts = async(req, res) => {
+    const workouts = await Workout.find({}).sort({createdAt: -1}) // get all object that's why this is blank
+
+    res.status(200).json(workouts)
+
+}
+
+// get a single workout
+const getWorkout = async(req, res) => {
+    const {id} = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(404).json({error: 'No such workout'})
+    }
+    const workout = await Workout.findById(id)
+    if(!workout)
+    {
+        return res.status(400),json({error: 'No such workout'})
+    }
+    
+    res.status(200).json(workout)
+    
+}
+
+// create new workouts
+const createWorkout = async(req, res) => {
+    const {title, load, reps} = req.body
+
+    // add doc to db
+    try {
+        const workout = await Workout.create({title, load, reps}) // this is an asynchronous
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+    res.json({mssg: 'post a new workout'})
+}
+
+// delete a workout
+const deleteWorkout = async(req, res) => {
+    const {id} = req.params
+    if (!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findOneAndDelete({_id: id})
+    if(!workout)
+    {
+        return res.status(400),json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
+
+// update a workout
+const updateWorkout = async(req, res) => {
+    const {id} = req.params
+    if (!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(404).json({error: 'No such workout'})
+    }
+    // spread the properties off that body which is an object
+    // because this method is asynchronous and returns a Promise. When you use await with a Promise, it tells JavaScript to pause execution of the function until the Promise resolves, ensuring that you have the complete result available before proceeding to the next line of code.
+    // So, If without await, updatedWorkout may not yet contain the resolved Promise result when res.json() is called, leading to potential issues like sending incomplete data or errors not being caught properly.
+    const workout = await Workout.findOneAndUpdate({_id: id}, {...req.body}, {new: true})
+
+    if(!workout)
+    {
+        return res.status(400),json({error: 'No such workout'})
+    }
+
+    res.status(200).json(workout)
+}
+
+
+
+// exports createWorkout inside the object
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,42 +1,30 @@
 const express = require('express')
 
-const Workout = require('../models/workoutModel')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
 
 // an instance of router forest
 const router = express.Router()
 
 // get all workouts
-router.get('/', (req, res)=>{
-    res.json({mssg: 'Get all workouts'})
-})
+router.get('/', getWorkouts)
 
 // get a single workout
-router.get('/:id', (req, res)=>{
-    res.json({mssg: 'Get a single workout'})
-})
+router.get('/:id', getWorkout)
 
 // post a new workout
-router.post('/', async(req, res)=>{
-    const {title, load, reps} = req.body
-    try {
-        const workout = await Workout.create({title, load, reps}) // this is an asynchronous
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-
-    res.json({mssg: 'post a new workout'})
-})
+router.post('/', createWorkout)
 
 // delete a workout
-router.delete('/:id', (req, res)=>{
-    res.json({mssg: 'delete a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // update a workout
-router.patch('/:id', (req, res)=>{
-    res.json({mssg: 'update a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 // at the end, we need to export our router
 module.exports = router
